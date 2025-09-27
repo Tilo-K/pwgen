31fb3ca25ea533a807b2a9ffadc2c011f8c56497
Tilo K <tiloklarenbeek@gmail.com>
2025-09-27 11:49:16 +0200

    feat: added error message

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::env;
 use rand::{distributions::Alphanumeric, Rng};
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut length = 16;
 
-    if args.len() >= 2{
-        length = args[1].parse().unwrap()
+    if args.len() >= 2 {
+        length = args[1]
+            .parse()
+            .expect(format!("Could not parse '{}' as an integer", args[1]).as_str());
     }
 
     let s: String = rand::thread_rng()
